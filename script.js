5ab62804073534c384d11cf2e4f467d100ede464
Alex Perez <aperez20@students.towson.edu>
2021-10-07 04:17:30 -0400

    insightful comment

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
-
 function button(){
         var e = document.getElementById("tilt");
         e.rotate(180);
         e.innerHTML = "WHY DID YOU DO THIS?!";
 }
+
+function clock(){
+
+//Save the times in variables
+
+var today = new Date();
+
+var hours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+
+
+//Set the AM or PM time
+
+if (hours >= 12){
+  meridiem = " PM";
+}
+else {
+  meridiem = " AM";
+}
+
+
+//convert hours to 12 hour format and put 0 in front
+if (hours>12){
+	hours = hours - 12;
+}
+else if (hours===0){
+	hours = 12;
+}
+
+//Put 0 in front of single digit minutes and seconds
+
+if (minutes<10){
+	minutes = "0" + minutes;
+}
+else {
+	minutes = minutes;
+}
+
+if (seconds<10){
+	seconds = "0" + seconds;
+}
+else {
+	seconds = seconds;
+}
+
+
+document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds + meridiem);
+
+}
+
+
+setInterval('clock()', 1000);
+
 Object.prototype.rotate = function(d)
 {
     var s = "rotate(" + d + "deg)";
@@ -25,7 +78,8 @@ document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
 function theDomHasLoaded(e) {
     var d = new Date();
     var curr_date = d.getDate();
-    var curr_month = d.getMonth() + 1;
+    var curr_month = d.getMonth() + 1; //months are zero based
     var curr_year = d.getFullYear();
     document.getElementById("date").innerHTML = (curr_month + "/" + curr_date);
+    clock();
 }
